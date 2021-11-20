a3333c78a6372b4a7e7719a42a0244a6b257cea1
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-20 14:20:43 +0800

    redis

--- src/main/java/com/kapcb/framework/middleware/configuration/CustomRedisAutoConfiguration.java
+++ src/main/java/com/kapcb/framework/middleware/configuration/CustomRedisAutoConfiguration.java
@@ -1,5 +1,8 @@
 package com.kapcb.framework.middleware.configuration;
 
+import cn.hutool.crypto.digest.DigestUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,14 +17,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a>Title: CustomRedisAutoConfiguration </a>
@@ -33,12 +49,30 @@ import org.springframework.data.redis.serializer.RedisSerializer;
  * @date 2021/11/12 22:02
  */
 @Slf4j
+@Configuration
+@EnableCaching
+@ConditionalOnClass(RedisOperations.class)
 @EnableConfigurationProperties(RedisConfigurationProperties.class)
 @ConditionalOnProperty(value = "kapcb.redis.enable", havingValue = "true", matchIfMissing = true)
-public class CustomRedisAutoConfiguration {
+public class CustomRedisAutoConfiguration extends CachingConfigurerSupport {
 
+    /**
+     * 设置 redis 数据默认过期时间默认为2小时
+     * 设置@Cacheable的序列化方式
+     *
+     * @return RedisCacheConfiguration
+     */
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        FastJsonRedisSerializer<Object> redisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer)).entryTtl(Duration.ofHours(2));
+        return configuration;
+    }
+
+    @SuppressWarnings("all")
     @Bean(value = "redisTemplate")
-    @ConditionalOnClass(RedisOperations.class)
+    @ConditionalOnMissingBean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         log.info("[ begin to add redis template into spring application context ]");
         // 关闭共享连接, 用于动态切换Redis数据库
@@ -76,5 +110,58 @@ public class CustomRedisAutoConfiguration {
     @ConditionalOnBean(value = RedisTemplate.class)
     public IRedisService iRedisService(@Qualifier("redisTemplate") RedisTemplate<String, Object> redisTemplate) {
         return new RedisServiceImpl(redisTemplate);
+    }
+
+    /**
+     * 异常处理，当Redis发生异常时，打印日志，但是程序正常走
+     *
+     * @return CacheErrorHandler
+     */
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler() {
+        log.info("initial -> [ Redis CacheErrorHandler ]");
+        return new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.error("Redis occur handleCacheGetError：key -> [{}]", key, e);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.error("Redis occur handleCachePutError：key -> [{}]；value -> [{}]", key, value, e);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+                log.error("Redis occur handleCacheEvictError：key -> [{}]", key, e);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.error("Redis occur handleCacheClearError：", e);
+            }
+        };
+    }
+
+    /**
+     * 自定义缓存key生成策略，默认将使用该策略
+     *
+     * @return KeyGenerator
+     */
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, param) -> {
+            Map<String, Object> container = new HashMap<>(2);
+            Class<?> targetClass = target.getClass();
+            container.put("class", targetClass);
+            container.put("methodName", method.getName());
+            container.put("package", targetClass.getPackage());
+            for (int i = 0; i < param.length; i++) {
+                container.put(String.valueOf(i), param[i]);
+            }
+            String jsonString = JSON.toJSONString(container);
+            return DigestUtil.sha256Hex(jsonString);
+        };
     }
 }
