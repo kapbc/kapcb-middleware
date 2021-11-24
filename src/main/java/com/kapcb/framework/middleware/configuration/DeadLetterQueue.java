d59dfede3b96b6b20b73368d55073113bed08b74
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-24 08:32:14 +0800

    add dead letter queue

---
+++ src/main/java/com/kapcb/framework/middleware/configuration/DeadLetterQueue.java
@@ -0,0 +1,13 @@
+package com.kapcb.framework.middleware.configuration;
+
+/**
+ * <a>Title: DeadLetterQueue </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: DeadLetterQueue <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/24 8:31
+ */
+public interface DeadLetterQueue {
+}
---
+++ src/main/java/com/kapcb/framework/middleware/configuration/RabbitDeadLetterQueueConfiguration.java
@@ -0,0 +1,15 @@
+package com.kapcb.framework.middleware.configuration;
+
+/**
+ * <a>Title: RabbitDeadLetterQueueConfiguration </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: RabbitDeadLetterQueueConfiguration <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/11/24 8:31
+ */
+public class RabbitDeadLetterQueueConfiguration implements DeadLetterQueue {
+
+
+}
