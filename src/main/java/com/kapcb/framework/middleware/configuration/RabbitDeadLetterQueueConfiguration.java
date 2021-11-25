31395824aed36a1c855a8a0a322818de478d565a
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-25 08:30:08 +0800

    rabbit

--- src/main/java/com/kapcb/framework/middleware/configuration/RabbitDeadLetterQueueConfiguration.java
+++ src/main/java/com/kapcb/framework/middleware/configuration/RabbitDeadLetterQueueConfiguration.java
@@ -1,5 +1,8 @@
 package com.kapcb.framework.middleware.configuration;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+
 /**
  * <a>Title: RabbitDeadLetterQueueConfiguration </a>
  * <a>Author: Kapcb <a>
@@ -9,7 +12,9 @@ package com.kapcb.framework.middleware.configuration;
  * @version 1.0.0
  * @date 2021/11/24 8:31
  */
-public class RabbitDeadLetterQueueConfiguration implements DeadLetterQueue {
+public abstract class RabbitDeadLetterQueueConfiguration implements DeadLetterQueue {
+
+    public abstract Queue deadQueue(String deadQueue, String deadExchange, long ttl);
 
 
 }
