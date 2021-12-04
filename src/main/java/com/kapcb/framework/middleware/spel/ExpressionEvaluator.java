75efb8cd6d5c0d776b514df1cd489be3c813961f
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-12-04 11:14:22 +0800

    spring expression

--- src/main/java/com/kapcb/framework/middleware/spel/ExpressionEvaluator.java
+++ src/main/java/com/kapcb/framework/middleware/spel/ExpressionEvaluator.java
@@ -1,13 +1,15 @@
 package com.kapcb.framework.middleware.spel;
 
+import org.springframework.context.expression.AnnotatedElementKey;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 import org.springframework.expression.Expression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
 
-import java.lang.reflect.AnnotatedElement;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * <a>Title: ExpressionEvaluator </a>
@@ -42,25 +44,35 @@ public class ExpressionEvaluator {
         return this.parameterNameDiscoverer;
     }
 
-    protected Expression getExpression(Map<ExpressionKey, Expression> cache) {
+    protected Expression getExpression(Map<ExpressionKey, Expression> cache, AnnotatedElementKey annotatedElementKey, String springExpression) {
+        ExpressionKey expressionKey = createExpressionKey(annotatedElementKey, springExpression);
+        Expression expression = cache.get(expressionKey);
+        if (Objects.isNull(expression)) {
+            expression = getExpressionParser().parseExpression(springExpression);
+            cache.put(expressionKey, expression);
+        }
+        return expression;
+    }
 
+    private ExpressionEvaluator.ExpressionKey createExpressionKey(AnnotatedElementKey annotatedElementKey, String expression) {
+        return new ExpressionKey(expression, annotatedElementKey);
     }
 
     protected static class ExpressionKey implements Comparable<ExpressionEvaluator.ExpressionKey> {
 
-        private final AnnotatedElement annotatedElement;
+        private final AnnotatedElementKey annotatedElementKey;
         private final String expression;
 
-        protected ExpressionKey(String expression, AnnotatedElement annotatedElement) {
+        protected ExpressionKey(String expression, AnnotatedElementKey annotatedElementKey) {
             Assert.notNull(expression, "expression must not be null");
-            Assert.notNull(annotatedElement, "annotated element must not be null");
+            Assert.notNull(annotatedElementKey, "annotated element key must not be null");
             this.expression = expression;
-            this.annotatedElement = annotatedElement;
+            this.annotatedElementKey = annotatedElementKey;
         }
 
         @Override
-        public int compareTo(ExpressionEvaluator.ExpressionKey other) {
-            int result = this.annotatedElement.toString().compareTo(other.annotatedElement.toString());
+        public int compareTo(ExpressionKey other) {
+            int result = this.annotatedElementKey.toString().compareTo(other.annotatedElementKey.toString());
             if (result == 0) {
                 result = this.expression.compareTo(other.expression);
             }
@@ -69,17 +81,24 @@ public class ExpressionEvaluator {
 
         @Override
         public int hashCode() {
-            return super.hashCode();
+            return this.annotatedElementKey.hashCode() * 29 + this.expression.hashCode();
         }
 
         @Override
         public boolean equals(Object obj) {
-            return super.equals(obj);
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ExpressionKey)) {
+                return false;
+            }
+            ExpressionKey expressionKey = (ExpressionKey) obj;
+            return this.annotatedElementKey.equals(expressionKey.annotatedElementKey) && ObjectUtils.nullSafeEquals(this.expression, expressionKey.expression);
         }
 
         @Override
         public String toString() {
-            return super.toString();
+            return this.annotatedElementKey + " with expression \"" + this.expression + "\"";
         }
     }
 
